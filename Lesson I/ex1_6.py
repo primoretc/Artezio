2fb7debfdd6600f135a0a38c2a1b60201763bbc4
primoretc <primoretc@gmail.com>
2020-01-23 11:41:51 +0300

    ex1_6.py DONE!!!

--- Lesson I/ex1_6.py
+++ Lesson I/ex1_6.py
@@ -3,3 +3,13 @@
 Sample Dictionary ( n = 5) :
 Expected Output : {1: 1, 2: 4, 3: 9, 4: 16, 5: 25} 
 """
+d = {}
+n = input("Число элементов словаря: ")
+n = int(n)
+i=1
+while i <= n:
+    d[i] = i**2
+    i+=1
+
+print(d)  
+
