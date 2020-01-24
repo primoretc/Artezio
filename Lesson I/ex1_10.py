dfa08e8027ae44660d23576b737d493795a037a2
primoretc <primoretc@gmail.com>
2020-01-24 14:38:20 +0300

    ex1_10.py DONE!!!

--- Lesson I/ex1_10.py
+++ Lesson I/ex1_10.py
@@ -2,3 +2,12 @@
 10. Write a Python program to get the difference between the two lists
 
 """
+
+l1 = ['Москва', 'Пекин', 'Торонто', 'Париж']   
+l2 = ['Москва', 'Пекин'] 
+if len(l1) > len(l2):
+    l3 = list(set(l1)-set(l2))
+elif  len(l1) < len(l2):
+    l3 = list(set(l2)-set(l1))
+
+print (l3)
