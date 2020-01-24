0e76aae711fd218eb61e88d133b4363cf34052e3
primoretc <primoretc@gmail.com>
2020-01-24 13:34:42 +0300

    ex1_9.py DONE!!!

--- Lesson I/ex1_9.py
+++ Lesson I/ex1_9.py
@@ -3,8 +3,14 @@
 
 """
 l = [1,3,4,3,1,56,1,56,234]
+#l = [1,2,3,4,5,6,7,8]
+#l = ['a','s','a','g']
 print(l)
+
 for i in l:
-     if  l.count(i)>=2:
-        l.remove(i)
+        for k in l:
+                if i == k and l.count(i) >=2:
+                        l.remove(k)
+
 print(l)
+
