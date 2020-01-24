d5d54349cc0c0c646ac4fb2b7a19e6db7897387a
primoretc <primoretc@gmail.com>
2020-01-24 15:52:06 +0300

    ex2_5.py DONE!!!

--- Lesson II/ex2_5.py
+++ Lesson II/ex2_5.py
@@ -6,3 +6,19 @@
 	"Please try again..."
 	И снова запрашивает пароль (не завершается).
 '''
+
+name = input("Введите имя пользователя: ")
+psw =  input("Введите пароль: ")
+
+PASSW = "qwerty"
+
+while True:
+    if psw == PASSW:
+       print(f"Password for user: {name} is correct") 
+       break
+    else:
+        print(f"Password for user: {name} is incorrect")
+        print("Please try again...")
+        psw =  input("Введите пароль: ")
+        continue
+
