77d2ec5e727b9488c586a0e4191b2e545fe96882
primoretc <primoretc@gmail.com>
2020-01-29 10:05:59 +0300

    Lesson III начало...

---
+++ Lesson III/ex3_4.py
@@ -0,0 +1,21 @@
+'''
+4. На вход функции передается строка с xml документом (тэги без аттрибутов, корневой элемент только один). 
+   Нужно выполнить следующее преобразование и вывести максимальную вложенность.
+   Пример: 
+        a = '<root><element1 /><element2 /><element3><element4 /></element3></root>'
+        foo(a) -> 
+        {
+            'name': 'root', 
+            'children': [
+                {'name': 'element1', 'children': []},
+                {'name': 'element2', 'children': []},
+                {
+                    'name': 'element3', 
+                    'children': [
+                        {'name': 'element4', 'children': []}
+                    ]
+                }
+            ]
+        }, 2
+      в данном случае у element4 тэга вложенность/глубина 2
+'''
