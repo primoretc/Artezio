055cd70df3fc470a85ae16f9b56591ed38eb49c3
primoretc <primoretc@gmail.com>
2020-01-30 11:23:49 +0300

    ex3_2.py in progress... *args OK

--- Lesson III/ex3_2.py
+++ Lesson III/ex3_2.py
@@ -11,20 +11,42 @@
 def nsum(*args, **kwargs):
     product = 1
     suma = 0
-    for i in args:
-        # надо вставить проверку чиссло это или нет и дальше принимать действия
-        if isinstance(i,int):
-            product = product * i
+    # def wtf(x, pr, su):
+    #     if isinstance(x,int):                   # если элемент число, то делаем сумму и произведение
+    #         pr = pr * x
+    #         print(f"product = {pr}")
+    #         su = su + x
+    #         print(f"suma = {su}")
+    #         return x, pr, su
+    #     elif isinstance(x,list):                # если элемент список то берем по одному элемиенту из него
+    #         for k, i in enumerate(x):
+    #             print (f"x = {x}")
+    #             wtf(x, pr, su )
+    def wtf(i):    
+        if isinstance(i,int):                   # если элемент число, то делаем сумму и произведение
+            nonlocal product
+            nonlocal suma
+            if i != 0: 
+                product = product * i
             print(f"product = {product}")
             suma = suma + i
             print(f"suma = {suma}")
+            return i
+        elif isinstance(i,list):                # если элемент список то берем по одному элемиенту из него
+            for k, i in enumerate(i):
+                wtf(i)
+        elif isinstance(i,tuple):                # если элемент кортеж то берем по одному элемиенту из него
+            for k, i in enumerate(i):
+                print (f"i = {i} type = {type(i)}")
+                wtf(i)                
+
+    for i in args:
+        wtf(i)
 
     for k, value in kwargs.items():
         print(k, value)
 
     return product, suma
         
-
-
 
 nsum(1, 2, 10, [3, 4, (5, 6, 0)], a=(10, 11), b=(3, 4, [5, 6, [7, 8], []]))
