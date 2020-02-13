9399ecfa68916552b16c7099c76589afb9060380
primoretc <primoretc@gmail.com>
2020-02-13 14:54:25 +0300

    ex4 DONE!!!

--- Lesson IV/main.py
+++ Lesson IV/main.py
@@ -1,10 +1,10 @@
 from website_alive.check_response import chk_res
 
 
-#url = input("Введите адрес сайта: ")
-url="http://google.ru"
+url = input("Введите адрес сайта: ")
+#url="http://google.ru"
 
-print(chk_res(url))
+
 if chk_res(url):
     print("Site alive!")
 else:
